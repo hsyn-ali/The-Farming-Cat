190b0413654548b037659c481e92791482832acb
hsyn-ali <syedhassanalishah3255@gmail.com>
2026-05-14 07:03:29 +0800

    farm timers changed

--- classes/Farms/sheephouse.cs
+++ classes/Farms/sheephouse.cs
@@ -8,7 +8,7 @@ public class SheepHouse : AnimalFarm
     public SheepHouse(Vector2 position) : base(position) { }
 
     protected override Texture2D Sprite => RunTime.SheepHouse;
-    protected override float WaitTime => 30f;
+    protected override float WaitTime => 60f;
     protected override Item ProducedItem => RunTime.WoolItem;
     protected override Item FeedItem => RunTime.SheepFeedItem;
     protected override string Title => "Sheep House";
--- obj/Debug/net10.0/game project.AssemblyInfo.cs
+++ obj/Debug/net10.0/game project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f518cc433ff338a51266e61abbf50f287940a2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f448a6994408a99d9a1a767388516781d00ca5")]
 [assembly: System.Reflection.AssemblyProductAttribute("game project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
