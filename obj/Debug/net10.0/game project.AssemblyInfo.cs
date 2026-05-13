7f518cc433ff338a51266e61abbf50f287940a2a
hsyn-ali <syedhassanalishah3255@gmail.com>
2026-05-14 06:40:31 +0800

    file name changes

--- obj/Debug/net10.0/game project.AssemblyInfo.cs
+++ obj/Debug/net10.0/game project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae302abdc14431ff3112d80e7e0546d1574911f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b2ce820e27efdd766509687f8fff6982efbfbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("game project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
